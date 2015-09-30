69e978503e44c968bd3d4f13710f27a83c0d35b7
Tin-Tin <kt-chin@hotmail.com>
2015-09-30 18:12:43 +0100

    Boundary
    Modified Boundary for ship

--- octet/src/examples/my_gameAssignment/my_gameAssignment.h
+++ octet/src/examples/my_gameAssignment/my_gameAssignment.h
@@ -230,25 +230,26 @@ namespace octet {
 			// left and right arrows
 			if (is_key_down(key_left)) {
 				sprites[ship_sprite].rotateMatrix(5);
-				if (sprites[ship_sprite].collides_with(sprites[first_border_sprite + 2])) {
-					sprites[ship_sprite].translate(+ship_speed, 0);
-				}
 			}
 			else if (is_key_down(key_right)) {
 				sprites[ship_sprite].rotateMatrix(-5);
-				if (sprites[ship_sprite].collides_with(sprites[first_border_sprite + 3])) {
-					sprites[ship_sprite].translate(-ship_speed, 0);
-				}
 			}
+
 			if (is_key_down(key_up)) {
 				sprites[ship_sprite].translate(0, +ship_speed);
-				if (sprites[ship_sprite].collides_with(sprites[first_border_sprite + 1])) {
+				if (sprites[ship_sprite].collides_with(sprites[first_border_sprite]) || 
+					sprites[ship_sprite].collides_with(sprites[first_border_sprite + 1]) || 
+					sprites[ship_sprite].collides_with(sprites[first_border_sprite + 2]) || 
+					sprites[ship_sprite].collides_with(sprites[first_border_sprite + 3])) {
 					sprites[ship_sprite].translate(0, -ship_speed);
 				}
 			}
 			else if (is_key_down(key_down)) {
 				sprites[ship_sprite].translate(0, -ship_speed);
-				if (sprites[ship_sprite].collides_with(sprites[first_border_sprite])) {
+				if  (sprites[ship_sprite].collides_with(sprites[first_border_sprite]) ||
+					sprites[ship_sprite].collides_with(sprites[first_border_sprite + 1]) ||
+					sprites[ship_sprite].collides_with(sprites[first_border_sprite + 2]) ||
+					sprites[ship_sprite].collides_with(sprites[first_border_sprite + 3])) {
 					sprites[ship_sprite].translate(0, +ship_speed);
 				}
 			}
